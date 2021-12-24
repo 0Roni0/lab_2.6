eb4db628c1a4a26ee2f0e165bb36731ed780f8c9
0Roni0 <84076559+0Roni0@users.noreply.github.com>
2021-12-24 12:21:20 +0300

    Add files via upload

---
+++ zadanie/idz1.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    # Список людей.
+    fellows = []
+
+    # Организовать бесконечный цикл запроса команд.
+    while True:
+        # Запросить команду из терминала.
+        command = input(">>> ").lower()
+
+        # Выполнить действие в соответствие с командой.
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            # Запросить данные о человеке.
+            name = input("Фамилия и инициалы? ")
+            number = input("Номер телефона? ")
+            from datetime import datetime
+            birthday = input("Enter your date of birth: ")
+            bday = datetime.strptime(birthday, '%d.%m.%Y')
+            # Создать словарь.
+            fellow = {
+                'name': name,
+                'number': number,
+                'birthday': birthday,
+            }
+            # Добавить словарь в список.
+            fellows.append(fellow)
+            # Отсортировать список в случае необходимости.
+            if len(fellows) > 1:
+                fellows.sort(key=lambda x: x.get('birthday')[::-1])
+
+        elif command == 'list':
+            # Заголовок таблицы.
+            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+                '-' * 4,
+                '-' * 30,
+                '-' * 20,
+                '-' * 15
+            )
+            print(line)
+            print(
+                '| {:^4} | {:^30} | {:^20} | {:^15} |'.format(
+                    "№",
+                    "Ф.И.О.",
+                    "Номер телефона",
+                    "Дата рождения"
+                )
+            )
+            print(line)
+
+            # Вывести данные о всех людях.
+            for idx, fellow in enumerate(fellows, 1):
+                print(
+                    '| {:>4} | {:<30} | {:<20} | {:>15} |'.format(
+                        idx,
+                        fellow['name'],
+                        fellow['number'],
+                        # переводим дату рождения в строку
+                        ' '.join((str(i) for i in fellow['birthday']))
+                    )
+                )
+            print(line)
+        elif command == 'whois':
+            who = input('Какой месяц?: ')
+
+            print(name)
+            print(number)
+            print(birthday)
+        elif command == 'help':
+            # Вывести справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить работника;")
+            print("list - вывести список работников;")
+            print("whois - вывести нужный месяц;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+        else:
+            print('Неизвестная команда', command, file=sys.stderr)
